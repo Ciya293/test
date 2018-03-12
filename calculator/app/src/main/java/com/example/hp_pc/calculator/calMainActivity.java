ce2ab2b6937b73cb4ad612f56700ca46dfc256d5
ciya293 <925233733@qq.com>
2018-03-12 15:02:33 +0800

    calculator页面布局

---
+++ calculator/app/src/main/java/com/example/hp_pc/calculator/calMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hp_pc.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class calMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cal_activity_cal__main);
+
+
+        }
+}
